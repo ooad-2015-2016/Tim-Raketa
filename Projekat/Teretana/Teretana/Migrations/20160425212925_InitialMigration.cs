c7a5595fc0c2f33114ca13a454da759d210e88ed
Omar Sokolovic <osokolovic1@etf.unsa.ba>
2016-04-25 23:46:07 +0200

    komentarisan autoincrement

--- Projekat/Teretana/Teretana/Migrations/20160425212925_InitialMigration.cs
+++ Projekat/Teretana/Teretana/Migrations/20160425212925_InitialMigration.cs
@@ -98,6 +98,7 @@ namespace TeretanaMigrations
                 });
         }
 
+
         public override void Down(MigrationBuilder migration)
         {
             migration.DropTable("Administrator");
